981bb20c746004ef077973672d7717bdcc704909
Ozioma EMEREM <code.emerem@gmail.com>
2023-08-11 12:53:02 +0100

    Add evidences

--- producer.py
+++ producer.py
@@ -1,41 +1,48 @@
-# import tweepy
+# Import necessary libraries fro tweepy
 from json import dumps
 import logging
-from datetime import datetime
 import json
 from dotenv import dotenv_values
 import tweepy
 import kafka
 
+# Set up logging config
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s ::%(levelname)s::%(name)s --> %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
+
+# Define the Kafka server address
+# NOTE THAT THIS COULL CHANGE. See readme
 server = '35.175.217.207'
 
+# Load configuration values from a .env file
 config = dotenv_values(".env")
 twitter_bearer_token = config["TWITTER_BEARER_TOKEN"]
+
+# Define the names of topics
+
 topics_name = ['Politics', 'Health', 'School']
 
+# Print the first topic name
 print(topics_name[0])
 
 def connect_to_kafka(server):
     try:
+        # Create a Kafka producer instance
         producer = kafka.KafkaProducer(
             bootstrap_servers=server,
-            # value_serializer = lambda x:dumps(x).encode('utf-8')
         )
         return producer
-    
+
     except kafka.errors.NoBrokersAvailable:
-        raise kafka.errors.NoBrokersAvailable("No brokers available at the specified url: {server}")
+        raise kafka.errors.NoBrokersAvailable(f"No brokers available at the specified url: {server}")
 
-
-def send_message(producer: kafka.KafkaProducer, topic: str, message:bytes):
+def send_message(producer: kafka.KafkaProducer, topic: str, message: bytes):
+    # Send a message to the specified Kafka topic
     producer.send(topic, message)
-
 
 class TwitterStreamingClient(tweepy.StreamingClient):
 
@@ -47,24 +54,31 @@ class TwitterStreamingClient(tweepy.StreamingClient):
         print(self.topic, self.producer)
 
     def on_data(self, raw_data):
-        # send_message(raw_data)
+        # Print and process raw data
         print(type(raw_data), raw_data)
-        
+
         data = json.loads(raw_data)
         print(data)
+
+        # Send the raw data to the Kafka topic
         send_message(self.producer, self.topic, raw_data)
 
     def on_connection_error(self):
+        # Handle connection error
         print(self.consumer_key)
-        logger.info("A connection error occured")
+        logger.info("A connection error occurred")
 
     def on_request_error(self, status_code):
+        # Handle request error
         print(self.consumer_key)
-        logger.info("An error of status {} occured".format(status_code))
+        logger.info(f"An error of status {status_code} occurred")
 
 try:
+    # Connect to Kafka server
     producer = connect_to_kafka(server)
-    stream_politics = TwitterStreamingClient(twitter_bearer_token , producer, topics_name[0])
+
+    # Set up Twitter streaming clients for different topics
+    stream_politics = TwitterStreamingClient(twitter_bearer_token, producer, topics_name[0])
     stream_politics.add_rules(tweepy.StreamRule(topics_name[0]))
     stream_politics.filter()
 
@@ -75,9 +89,13 @@ try:
     stream_school = TwitterStreamingClient(twitter_bearer_token, producer, topics_name[2])
     stream_school.add_rules(tweepy.StreamRule(topics_name[2]))
     stream_school.filter()
+
 except KeyboardInterrupt:
+    # Delete streaming rules if interrupted
     stream_politics.delete_rules(topics_name[0])
     stream_politics.delete_rules(topics_name[1])
     stream_politics.delete_rules(topics_name[2])
+
 except Exception as e:
-        print(e)
+    # Print any other exceptions that occur
+    print(e)
--- topic.py
+++ topic.py
@@ -1,7 +1,8 @@
-from kafka.admin import KafkaAdminClient, NewTopic, ConfigResource, ConfigResourceType 
+# Import necessary libraries for managing Kafka topics
+from kafka.admin import KafkaAdminClient, NewTopic, ConfigResource, ConfigResourceType
 import logging
 
-
+# Set up logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s ::%(levelname)s::%(name)s --> %(message)s",
@@ -9,45 +10,52 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-
-
+# Create a Kafka admin client instance
 admin_client = KafkaAdminClient(bootstrap_servers='35.175.217.207')
+
+# Define the names of topics
 topics_name = ['Politics', 'Health', 'School']
 
+# Define the number of partitions and replicas for topics
 partitions = 1
 replicas = 1
+
+# Initialize a list to store NewTopic instances
 kafka_topic_name = list()
 
+# Iterate through topic names
 for i in topics_name:
     try:
+        # Check if the topic doesn't exist
         if i not in admin_client.list_topics():
-            logger.info(f"New topic : `{i}` is added")
+            logger.info(f"New topic: `{i}` is added")
             kafka_topic_name.append(
                 NewTopic(
                     name=i,
-                    num_partitions=partitions, 
+                    num_partitions=partitions,
                     replication_factor=replicas
-                    )
+                )
             )
         else:
-            logger.warning(f"The topic [{i}]is existing!" )
+            logger.warning(f"The topic [{i}] is existing!")
     except Exception as e:
         logger.warning(e)
 
 try:
+    # Create new topics if they don't exist
     if kafka_topic_name != admin_client.list_topics():
         admin_client.create_topics(new_topics=kafka_topic_name)
     else:
-        logger.warning(f"The topics existing!" )
+        logger.warning("The topics already exist!")
 except Exception as e:
-    logger.warning("Error creating topics !")
+    logger.warning("Error creating topics!")
     logger.warning(e)
-        
-# logger.info(admin_client.delete_topics(topics=topics_name))
 
-# logger.info(admin_client.list_topics())
+# List consumer groups
 logger.info(admin_client.list_consumer_groups())
+
+# logger.info(admin_client.delete_topics(topics=topics_name))
+# logger.info(admin_client.list_topics())
 # logger.info(admin_client.describe_cluster())
 # logger.info(admin_client.describe_topics(["Politics"]))
-
 # logger.info(admin_client.describe_configs(config_resources=[ConfigResource(ConfigResourceType.TOPIC, "Politics")]))
